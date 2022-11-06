a80c1840adb5cb9b3054b056e9c5d55498066117
Josue <josgar_14@hotmail.com>
2022-11-06 05:30:28 -0400

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,20 +12,14 @@ void main() {
   );
 }
 
-Stream<String> getTime() => Stream.periodic(
-      const Duration(seconds: 1),
-      (_) => DateTime.now().toIso8601String(),
-    );
-
 class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final dateTime = useStream(getTime());
     return Scaffold(
       appBar: AppBar(
-        title: Text(dateTime.data ?? 'Home Page'),
+        title: const Text('Home Page'),
       ),
     );
   }
